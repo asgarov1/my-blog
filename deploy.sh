4eb8476e899513d69ac211498ef5d640e08ee646
Javid Asgarov <asgarov1@gmail.com>
2023-03-10 07:02:01 +0100

    changes in deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,5 @@
 ng build -c production;
 ssh -i ~/Documents/keys/blog.pem ubuntu@ec2-3-75-240-39.eu-central-1.compute.amazonaws.com "rm -rf ./blog/*";
 scp -r -i ~/Documents/keys/blog.pem ./dist/blog/* ubuntu@ec2-3-75-240-39.eu-central-1.compute.amazonaws.com:/home/ubuntu/blog/;
+ssh -i ~/Documents/keys/blog.pem ubuntu@ec2-3-75-240-39.eu-central-1.compute.amazonaws.com "sudo rm -rf /var/www/html/blog/*";
+ssh -i ~/Documents/keys/blog.pem ubuntu@ec2-3-75-240-39.eu-central-1.compute.amazonaws.com "sudo cp -r ./blog/* /var/www/html/blog/";
